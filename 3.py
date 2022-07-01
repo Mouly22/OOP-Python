f10cda629b43a563bbcbd6807e1a951fb015e837
Mouly <abiraazmary22@gmail.com>
2022-07-01 16:13:36 +0600

    ...

---
+++ 3.py
@@ -0,0 +1,21 @@
+class Flower:
+    def __init__(self):
+        self.name = ''
+        self.color = ''
+        self.num_of_petal = 0
+
+flower1 = Flower()
+flower1.name="Rose"
+flower1.color="Red"
+flower1.num_of_petal=6
+print("Name of this flower:", flower1.name)
+print("Color of this flower:",flower1.color)
+print("Number of petal:",flower1.num_of_petal)
+print('=====================')
+flower2 = Flower()
+flower2.name="Orchid"
+flower2.color="Purple"
+flower2.num_of_petal=4
+print("Name of this flower:",flower2.name)
+print("Color of this flower:",flower2.color)
+print ("Number of petal:",flower2. num_of_petal)
