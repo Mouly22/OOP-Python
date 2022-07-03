4c1316a7b5521878a710336280f380dc2ca226b9
Mouly <abiraazmary22@gmail.com>
2022-07-03 21:58:28 +0600

    ....

---
+++ 4.py
@@ -0,0 +1,25 @@
+class Joker:
+    def __init__(self, name, power, is_he_psycho):
+        self.name = name
+        self. power = power
+        self.is_he_psycho = is_he_psycho
+        
+j1 = Joker('Heath Ledger', 'Mind Game', False)
+print(j1.name)
+print(j1.power)
+print(j1.is_he_psycho)
+print('=====================')
+j2 = Joker('Joaquin Phoenix', 'Laughing out Loud', True)
+print(j2.name)
+print(j2.power)
+print(j2.is_he_psycho)
+print('=====================')
+if j1 == j2:
+    print('same')
+else:
+    print('different')
+j2.name = 'Heath Ledger'
+if j1.name == j2.name:
+    print('same')
+else:
+    print('different')
