61a4bcef828a618207e2c3e828c1ad8c10c5d920
Mouly <abiraazmary22@gmail.com>
2022-07-13 09:39:28 +0600

    ........

---
+++ 7.py
@@ -0,0 +1,21 @@
+class Country:
+    def __init__(self, name = 'Bangladesh', continent = 'Asia', capital = "Dhaka", fifa_ranking = 187):
+        self.name = name
+        self.continent = continent
+        self.capital = capital
+        self.fifa_ranking = fifa_ranking
+        
+country = Country()
+print('Name:',country.name)
+print('Continent:',country.continent)
+print('Capital:',country.capital)
+print('Fifa Ranking:',country.fifa_ranking)
+print('===================')
+country.name = 'Belgium'
+country.continent = 'Europe'
+country.capital = 'Brussels'
+country.fifa_ranking = 1
+print('Name:',country.name)
+print('Continent:',country.continent)
+print('Capital:',country.capital)
+print('Fifa Ranking:',country.fifa_ranking)
