9895edf4af0fc9da01ed4e0f5d26e806e62eccf0
Mouly <abiraazmary22@gmail.com>
2022-10-01 01:43:12 +0600

    .....

---
+++ 75.py
@@ -0,0 +1,38 @@
+class Fruit:
+    def __init__(self, formalin=False, name=''):
+        self.__formalin = formalin
+        self.name = name
+    def getName(self):
+        return self.name
+    def hasFormalin(self):
+        return self.__formalin
+    
+class testFruit:
+    def test(self, f):
+        print('----Printing Detail----')
+        if f.hasFormalin():
+            print('Do not eat the',f.getName(),'.')
+            print(f)
+        else:
+            print('Eat the',f.getName(),'.')
+            print(f)
+            
+class Mango(Fruit, testFruit):
+    def __init__(self, formalin = True, name = 'Mango'):
+        super().__init__(formalin = formalin, name = name)
+        
+    def __str__(self):
+        return f'{super().getName()}s are bad for you'
+    
+class Jackfruit(Fruit, testFruit):
+    def __init__(self, formalin = False, name = 'Jackfruit'):
+        super().__init__(formalin = formalin, name = name)
+            
+    def __str__(self):
+        return f'{super().getName()}s are good for you'
+
+m = Mango()
+j = Jackfruit()
+t1 = testFruit()
+t1.test(m)
+t1.test(j)
